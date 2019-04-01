631dd8fced05e1f95ebba262f761c93167045c82
alisalehi <alisalehi1995@gmail.com>
2019-04-02 01:41:15 +0430

    progress in traits

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,18 +1,37 @@
 pub trait Summary {
-    fn summarize(&self) -> String;
-}
-
-impl Summary for Vec {
     fn summarize(&self) -> String {
-        let mut sum: u32 = 0;
-        for &elem in self {
-            sum += elem;
-        }
-        sum
+        String::from("Read more...")
     }
 }
 
+impl Summary for Vec<i32> {
+    fn summarize(&self) -> String {
+        let mut sum: i32 = 0;
+        for &elem in self {
+            sum += elem;
+        }
+        sum.to_string()
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {}
+
 
 fn main() {
+    let tweet = Tweet {
+        username: String::from("alisalehi"),
+        content: String::from("Apparently, the rule:) did not apply to me"),
+        reply: false,
+        retweet: false,
+    };
 
+    println!("tweet summarize: {}", tweet.summarize());
+    println!("{}", tweet.content);
 }
